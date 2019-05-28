2ff065d31336ff27bdb4bb1bf5579bdf99e22303
Nelson Mwirumubi <mwinel@live.com>
2019-05-28 11:44:25 +0300

    chore(testing-enviroment): Add testing enviroment setup
    - Install enzyme
    - Install react-test-renderer
    - Install enzyme-to-json
    - Install enzyme-adapter-react-16
    
    [Starts #165273425]

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import App from '../App';
+import toJson from 'enzyme-to-json'; // for snapshotting the component
+import { shallow } from 'enzyme';
+
+describe('<App />', () => {
+  let wrapper;
+
+  it('renders without crashing', () => {
+    wrapper = shallow(<App />);
+  });
+
+  it('matches snapshot', () => {
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+});
