7b755dbeed63f64fd4ccbba3e981f769515ebb61
sanya-kenneth <sanyakenneth@gmail.com>
2019-05-28 10:13:45 +0300

    ch(setup-sass): setup and install sass
    - Install and setup sass for the project
    - Convert existing css files to sass
    - setup and install bootstrap to the project
    
    [Starts #165273428]

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import $ from "jquery";
+import Popper from "popper.js";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
