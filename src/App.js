f6f82e03dbbc2a8ee7f4cbd75a0d2d1fcb50ef0d
3Nakajugo <edinahmargarita@gmail.com>
2019-05-28 09:25:01 +0300

    chore(setup routing): Setup react routing V4
    - install react-router-dom
    - create app directory
    - create router.jsx
    - update package.json
    - update snapshot
    - uninstall jquery
    - delete App directory
    - write tests
    
    [Starts #165273427]

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './App.scss';
+import Router from './Router';
 
 class App extends Component {
   render () {
     return (
       <Provider store={store}>
         <div className="App">
-          <div className="row">
-            <div className="col-md-4" />
-            <p className="card col-md-4 company_name">Author's Haven</p>
-            <div className="col-md-4" />
-          </div>
+          <Router />
         </div>
       </Provider>
+
     );
   }
 }
---
+++ src/app/Login/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render () {
+    return (
+      <div>
+        <h5>Authors Haven Login</h5>
+      </div>
+    );
+  }
+}
+
+export default Login;
